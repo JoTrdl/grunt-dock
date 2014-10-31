83424a4187a3c0984a32ca2fa7a84420d83660fc
Johann Troendle <johann.troendle@gmail.com>
2014-10-31 16:19:08 -0400

    Tasks list, clean (test, not working), build (not finished)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,9 @@
-var Docker = require('dockerode');
-var fs = require('fs');
-
-var defaultOptions = {
-  protocol: 'http',
-  host: '192.168.59.103',
-  port: '2376'
-};
-
-var commands = {};
-
-commands['list'] = {};
-commands['list']['images'] = function(grunt) {
-
-  var options = this.options(defaultOptions);
-  var docker = new Docker(options);
-
-  var done = this.async();
-
-    // 12
-  
-
-
-  grunt.log.subhead('REPOSITORY:TAG')
-  docker.listImages({all: false}, function(err, images) {
-    grunt.log.writeln(JSON.stringify(images));
-    for (i in images) {
-      for (j in images[i].RepoTags) {
-        grunt.log.writeln(images[i].RepoTags[j], "   ", images[i].Id.split("").slice(0,12).join(""));
-      }
-    }
-    grunt.verbose.ok();
-    done();
-  });
-};
-
 'use strict';
+
+
+var Docker = require('dockerode'),
+    fs = require('fs');
+
 
 /*
  * Commands:
@@ -49,7 +18,7 @@ commands['list']['images'] = function(grunt) {
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    docker: {
+    dock: {
       // Apply to All
       options: {
         protocol: 'https',
@@ -69,37 +38,53 @@ module.exports = function(grunt) {
           port: '2376',*/
 
           tag: 'altar/dev',
-          dockerfile: 'Dockerfile.tar'
+          dockerfile: 'Dockerfile'
         }
       }
     }
   });
 
 
-  grunt.task.registerTask('docker:list', 'List images/containers', function(arg) {
+  var commands = {
+    list:  { handler: require('./lib/list'),  description: 'List images/containers' },
+    clean: { handler: require('./lib/clean'), description: 'Clean old images/containers' },
+    build: { handler: require('./lib/build'), description: 'Build an image' }
+  };
+
+  /**
+   * List / Clean Grunt Task.
+   * 
+   * Usage:
+   *   docker:list:image | docker:list:container
+   *   docker:clean:image | docker:clean:container
+   *   
+   */
+  ['list', 'clean'].forEach(function(command) {
+    grunt.task.registerTask('dock:' + command, commands[command].description, function(arg) {
     
-    // Fake the default Grunt this.options
-    this.options = function(options) {
-      var config = {};
-      for (var name in grunt.config.data.docker.options) {
-        config[name] = grunt.config.data.docker.options[name];
-        if (!config[name]) config[name] = options[name]; 
+      // Fake the default Grunt this.options
+      this.options = function(options) {
+        var config = {};
+        for (var name in grunt.config.data.docker.options) {
+          config[name] = grunt.config.data.docker.options[name];
+          if (!config[name]) config[name] = options[name];
+        }
+        return config;
+      };
+
+      var func = commands[command].handler[arg];
+      if (!func) {
+        grunt.fail.fatal('Command [' + command + ':' + arg + '] not recognized.');
       }
-      return config;
-    };
 
-    var func = commands['list'][arg];
-    if (!func) {
-      grunt.fail.fatal('Command [list:' + arg + '] not recognized.');
-    }
-
-    func.apply(this, [grunt]);
-  
+      func.apply(this, [grunt]);
+    });
   });
 
-  grunt.registerMultiTask('docker', 'Docker management', function(command, arg) {
+
+  grunt.registerMultiTask('dock', 'Docker management', function(command, arg) {
    
-    
+    grunt.log.writeln(JSON.stringify(commands));
 
     grunt.log.writeln('command:', command, ', arg:', arg);
 
@@ -109,14 +94,14 @@ module.exports = function(grunt) {
     }
 
     // Check arg
-    if (typeof(commands[command]) != 'function') {
-      if (!commands[command][arg]) {
+    if (typeof(commands[command].handler) != 'function') {
+      if (!commands[command].handler[arg]) {
         grunt.fail.warn('Argument [' + arg + '] for [' + command + '] not recognized.');
         return;
       }
     }
 
-    var func = (arg) ? commands[command][arg] : commands[command];
+    var func = (arg) ? commands[command].handler[arg] : commands[command].handler;
     func.apply(this, [grunt]);
     
 
