ac963c6a46ec5b413fde30d2948657bcb2eac580
JoTrdl <johann.troendle@gmail.com>
2014-11-05 20:24:54 -0500

    New docker connection config

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,13 +21,17 @@ module.exports = function(grunt) {
     dock: {
       // Apply to All
       options: {
-        protocol: 'https',
-        host: '192.168.59.103',
-        port: '2376',
 
-        ca: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/ca.pem'),
-        cert: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/cert.pem'),
-        key: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/key.pem')
+        docker: {
+          protocol: 'https',
+          host: '192.168.59.103',
+          port: '2376',
+
+          ca: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/ca.pem'),
+          cert: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/cert.pem'),
+          key: fs.readFileSync('/Users/JohannTDL/.boot2docker/certs/boot2docker-vm/key.pem')
+        }
+        
       },
 
       // Dev
