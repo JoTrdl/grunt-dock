cfadc876826b6e10187c5bd34d886605a57408e6
Johann Troendle <johann.troendle@gmail.com>
2014-11-26 16:30:15 -0500

    Finished last sample

--- examples/nginxproxy/bundle/node/app.js
+++ examples/nginxproxy/bundle/node/app.js
@@ -3,7 +3,19 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<html><body>');
-  res.end('<h1>Hello World from node app</h1>');
+  res.write('<h1>Hello World from node app</h1>');
+  
+  
+  res.write('<ul>');
+  for (i in req.headers) {
+    res.write('<li>');
+    res.write(i + '&nbsp;:&nbsp;' + req.headers[i]);
+    res.write('</li>');
+  }
+  res.write('</ul>');
+
+  res.write('<p>Behind NGINX? ==> ' + (req.headers['x-forwarded-host'] == 'nginx') +  '</p>');
+
   res.write('</body></html>');
   res.end();
 });
